e76a3349663111a70c8c819a0704fdc9dead66ef
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2025-10-29 19:11:33 -0600

    initialize React app with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center justify-center gap-10">
+      <h1 className="text-4xl">Hola Mundo</h1>
+      <p className="code-font text-2xl animate-fade-in">Bloque de Código</p>
+    </div>
+  );
+};
+
+export default App;
